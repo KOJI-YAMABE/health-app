7c5ddbfb82811ae0af5c27358b0aa36774a5b3bc
Koji Yamabe <koji.yamabe@aakel.co.jp>
2025-01-11 16:16:34 +0900

    fix: page path

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { TopPage } from './pages/TopPage'
-import { MyPage } from './pages/MyPage'
+import { MyRecord } from './pages/MyRcord'
 import { ColumnsPage } from 'pages/ColumnsPage'
 
 const App = () => {
@@ -9,7 +9,7 @@ const App = () => {
         <BrowserRouter>
             <Routes>
                 <Route path={`/top`} element={<TopPage />} />
-                <Route path={`/mypage`} element={<MyPage />} />
+                <Route path={`/my_record`} element={<MyRecord />} />
                 <Route path={`/columns`} element={<ColumnsPage />} />
             </Routes>
         </BrowserRouter>
---
+++ src/pages/MyRcord.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const MyRecord = () => {
+    return (
+        <div>MyRecord</div>
+    )
+}
